ef014b75e8f06c1777221a9061c30a10ec39da0b
Suraj Sunar <surbhu06@gmail.com>
2026-03-27 21:12:42 +0600

    app: adding ocr

--- src/scanner/ocr.service.ts
+++ src/scanner/ocr.service.ts
@@ -1,12 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import Tesseract from 'tesseract.js';
 
+export type ExtractData = {
+  name: string;
+  idNumber: string;
+  raw: string;
+  image: string;
+};
+
 @Injectable()
 export class OCRService {
+  private uploadList: ExtractData[] = [];
+
+  getUploadList() {
+    return this.uploadList;
+  }
+
   async processImage(base64Image: string) {
     const result = await Tesseract.recognize(base64Image, 'eng');
     const text = result.data.text;
-    return this.extractFields(text, base64Image);
+    const extractedResults = this.extractFields(text, base64Image);
+
+    if (this.validateCID(extractedResults.idNumber) > -1) {
+      return {
+        idNumber: extractedResults.idNumber,
+        duplicate: true,
+      };
+    } else {
+      this.uploadList.unshift(extractedResults);
+      return extractedResults;
+    }
   }
 
   extractFields(text: string, base64Image: string) {
@@ -16,5 +39,9 @@ export class OCRService {
       raw: text,
       image: base64Image,
     };
+  }
+
+  private validateCID(cid: string) {
+    return this.uploadList.findIndex((row) => row.idNumber === cid);
   }
 }
