06f6fd9b1007b32f1972d51e075e14f3796f7066
Suraj Sunar <surbhu06@gmail.com>
2026-04-04 21:28:49 +0600

    app: added email validation yahoo

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -48,7 +48,7 @@ export class UserService {
 
   validateEmailDomain(email: string) {
     //validate with email as gmail
-    return email.length && ['@gmail', 'hotmail'].includes(email);
+    return email.length && ['@gmail', 'hotmail', '@yahoo'].includes(email);
   }
 
   async getUserByUserName(userName: string) {
